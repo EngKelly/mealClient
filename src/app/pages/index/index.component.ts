cd564ff16490d2b67d6547dda451930d67a54efd
kellyncode <eng.kelechiamos@gmail.com>
2023-07-31 11:54:18 +0100

    fixing: styling errors

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,3 @@
-import { ProductService } from './../../services/product/product.service';
 import { ProductDto } from './../../data/Dto/product/product.dto';
 import { UserDto } from '../../data/Dto/auth/user.dto';
 import { Component, HostListener, Inject } from '@angular/core';
@@ -15,8 +14,7 @@ export class IndexComponent {
   constructor(
     private jwtService: JwtService,
     private userService: UserService,
-    @Inject(ActivatedRoute) private activeRoute: ActivatedRoute,
-    private productService: ProductService
+    @Inject(ActivatedRoute) private activeRoute: ActivatedRoute
   ) {}
 
   userToken: any = this.jwtService.decodeJwtToken();
@@ -38,7 +36,6 @@ export class IndexComponent {
         console.log('Error getting the current logged in user');
       },
     });
-    this.getProducts('', 1);
     this.productId = this.activeRoute.snapshot.params['id'];
     this.activeBtn();
   }
@@ -56,25 +53,9 @@ export class IndexComponent {
     } else {
       this.IsMobile = true;
     }
-    console.log(windowWidth);
-    console.log(this.IsMobile);
   }
 
   activeBtn(): void {
     this.IsActive = !this.IsActive;
-  }
-
-  getProducts(productCategory: string = '', page: number = 1): void {
-    this.IsFetching = true;
-    this.productService.getProducts(productCategory, page).subscribe({
-      next: (response) => {
-        this.products = response.data;
-        this.IsFetching = false;
-      },
-      error: (err) => {
-        this.IsFetching = false;
-        console.error('Error occurred while fetching the user.', err.message);
-      },
-    });
   }
 }
